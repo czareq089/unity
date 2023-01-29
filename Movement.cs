67cf8ccf0271638f995ce10b7b62e32ecf4c7329
czareq089 <86021893+czareq089@users.noreply.github.com>
2023-01-29 20:53:39 +0100

    dodanie możliwości sprintu

--- Movement.cs
+++ Movement.cs
@@ -7,34 +7,39 @@ using UnityEngine.UIElements;
 
 public class Movement : MonoBehaviour
 {
-    public  float speed = 8f;
+    public float speed = 8f;
     private Rigidbody2D rb;
     private float inputX;
     public float limit = 16f;
+    public float sprint_speed = 12f;
 
 
     void Start()
     {
-         rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
         inputX = Input.GetAxis("Horizontal");
-        Debug.Log(rb.angularVelocity);
     }
     void FixedUpdate()
     {
-            rb.AddForce(new Vector2(inputX * speed, 0));
+        rb.AddForce(new Vector2(inputX * speed, 0));
 
-            if(inputX == 0)
-            {
-                rb.angularVelocity = 0;
-            }
+        if (inputX == 0)
+        {
+            rb.angularVelocity = 0;
+        }
 
-            if(rb.velocity.magnitude > limit)
-            {
-                rb.angularVelocity = limit;
-            }
+        if (rb.velocity.magnitude > limit)
+        {
+            rb.angularVelocity = limit;
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            rb.AddForce(new Vector2(inputX * sprint_speed, 0));
+        }
     }
 }
 
